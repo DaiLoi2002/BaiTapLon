c4ff31bc120ecf3c7bef042842967791bcb630bb
DaiLoi2002 <loidai2002@gmail.com>
2023-10-30 08:02:51 +0700

    thêm stack

--- demo/App.js
+++ demo/App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+import DangKi from './src/DangKi';
+import DangNhap from './src/DangNhap';
+import Home from './src/Home';
+
+
+const Stack = createNativeStackNavigator();
+
+
+
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="DangNhap" component={DangNhap} options={{headerShown: false}} />
+        <Stack.Screen name="DangKi" component={DangKi} options={{headerShown: false}} />
+        <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
